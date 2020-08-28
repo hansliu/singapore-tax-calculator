ab1d765d683f233c2872b535d6e8c7cb28e9508a
hansliu <hans@wego.com>
2020-08-28 23:54:30 +0800

    feat: initial

---
+++ singaporetax.js
@@ -0,0 +1,99 @@
+// $(document).ready(function(){
+//   $("#net_income").mask("9 999 999 999", { placeholder: " " } );
+// });
+
+var app = new Vue({
+    el: '#wrapper',
+    data: {
+
+        // USER ENTRIES
+        ui_salary: 67000,
+        salary: 0,
+        previous_salary: 0,
+
+        // CALCULATIONS
+        income_tax: 1,
+        income_tax_amount: 0,
+        income_net: 0,
+
+        // PERCENT OF SALARY
+        income_net_percent: 0,
+        income_tax_percent: 0,
+    },
+
+    mounted: function() {
+        this.calculate_all();
+    },
+
+    watch: {
+        ui_salary: function(newSalary) {
+            this.calculate_all();
+        }
+    },
+    methods: {
+
+        calculate_all: function() {
+            this.salary = parseInt(this.ui_salary);
+            this.previous_salary = this.salary;
+            this.taxable_income = this.salary;
+            this.calculate_income_tax_amount();
+            this.calculate_income_net();
+        },
+
+        calculate_income_tax_amount: function() {
+            var taxable_income = this.taxable_income;
+            this.income_tax_amount = 0;
+
+            if (taxable_income >= 320000) {
+                this.income_tax_amount = (taxable_income - 320000) * 0.22 + 44550;
+            }
+            else if (taxable_income >= 280000) {
+                this.income_tax_amount = (taxable_income - 280000) * 0.20 + 36550;
+            }
+            else if (taxable_income >= 240000) {
+                this.income_tax_amount = (taxable_income - 240000) * 0.195 + 28750;
+            }
+            else if (taxable_income >= 200000) {
+                this.income_tax_amount = (taxable_income - 200000) * 0.19 + 21150;
+            }
+            else if (taxable_income >= 160000) {
+                this.income_tax_amount = (taxable_income - 160000) * 0.18 + 13950;
+            }
+            else if (taxable_income >= 120000) {
+                this.income_tax_amount = (taxable_income - 120000) * 0.15 + 7950;
+            }
+            else if (taxable_income >= 80000) {
+                this.income_tax_amount = (taxable_income - 80000) * 0.115 + 3350;
+            }
+            else if (taxable_income >= 40000) {
+                this.income_tax_amount = (taxable_income - 40000) * 0.07 + 550;
+            }
+            else if (taxable_income >= 30000) {
+                this.income_tax_amount = (taxable_income - 30000) * 0.035 + 200;
+            }
+            else if (taxable_income >= 20000) {
+                this.income_tax_amount = (taxable_income - 20000) * 0.02;
+            }
+
+            this.income_tax_percent = Math.round(this.income_tax_amount / this.salary * 100);
+        },
+
+        calculate_income_net: function() {
+            this.income_net = this.salary - this.income_tax_amount;
+            this.income_net_percent = Math.round(this.income_net / this.salary * 100);
+            //console.debug("Income Net: ", this.income_net);
+        },
+
+    },
+
+    filters: {
+
+        to_sgd: function(value) {
+            if (!value)
+            return 'S$0';
+            var val = Math.round(value);
+            return "S$" + val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        }
+
+    }
+})
