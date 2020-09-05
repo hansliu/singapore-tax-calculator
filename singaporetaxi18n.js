54d883b7e5c74e2310868567e563c803c4c13843
hansliu <hansliu.tw@gmail.com>
2020-09-05 18:53:41 +0800

    rewording, add salary i18n

--- singaporetaxi18n.js
+++ singaporetaxi18n.js
@@ -4,7 +4,10 @@ const messages = {
         "content": "Welcome to the Singapore Tax Calculator. This website has been made for you to quickly get an idea of the amount of taxes you might have to pay especially for the work visa holders. Refer to Singapore IRAS to know exactly how taxation works for you. Feel free to participate in the comments at the bottom of this page. As of 2019, the average salary a year in Singapore is S$ 67,152."
       },
       "salary": {
-        "title": "How much do you make {ayear}? {expenses}"
+        "title": "How much do you make {ayear}? {expenses}",
+        "ayear": "a year",
+        "expenses": "(after expenses, if any)",
+        "net_income": "Your net income is:"
       },
       "about": {
         "title": "About the {website}",
@@ -17,14 +20,17 @@ const messages = {
     },
     "zh-tw": {
       "intro": {
-        "content": "歡迎來到 Singapore Tax Calculator。本網站想用最簡單的方式，讓想來新加坡工作或是已經在新加坡工作的工作簽證持有者，能快速瞭解您需要繳交多少個人所得稅。如果想知道更詳細的稅務資訊，請參考新加坡 IRAS 網站。如果對網站有什麼想法，也歡迎到最下方留言區留下您寶貴的意見。此外，根據統計，2019年新加坡人的平均年收入為 S$ 67,152 元"
+        "content": "歡迎來到 Singapore Tax Calculator。本網站想用最簡單的方式，讓想來新加坡工作或是已經在新加坡工作的工作簽證持有者，能快速瞭解你需要繳交多少個人所得稅。如果想知道更詳細的稅務資訊，請參考新加坡 IRAS 網站。如果對網站有什麼想法，也歡迎到最下方留言區留下你的意見。此外，根據統計，2019年新加坡的平均年收入為 S$ 67,152 元"
       },
       "salary": {
-        "title": "您{ayear}賺多少錢? {expenses}"
+        "title": "你{ayear}賺多少錢? {expenses}",
+        "ayear": "一年",
+        "expenses": "(扣除公務減免)",
+        "net_income": "你的淨收入是:"
       },
       "about": {
         "title": "關於 {website}",
-        "content": "Singapore Tax Calculator 網站由 {author} 開發與維護。網站的頁面設計與程式碼參考自 {thanks_website} (由 {thanks_author} 和 Emi 開發與維護)，如果有任何計算上的錯誤，歡迎您直接檢查程式碼。如果有什麼新想法，在保持盡可能簡單的設計情況下，也歡迎到 {github} 提出建議或請求。"
+        "content": "Singapore Tax Calculator 網站由 {author} 開發與維護。網站的頁面設計與程式碼參考自 {thanks_website} (由 {thanks_author} 和 Emi 開發與維護)，如果有任何計算上的錯誤，歡迎你直接檢查程式碼。如果有什麼新想法，在保持盡可能簡單的設計情況下，也歡迎到 {github} 提出建議或請求。"
       },
       "disclaimer": {
         "title": "免責聲明",
