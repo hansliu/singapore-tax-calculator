4136749db8eb3fa267746c050178284e94200985
hansliu <hans@wego.com>
2020-09-17 17:42:08 +0800

    add icon made by

--- singaporetaxi18n.js
+++ singaporetaxi18n.js
@@ -11,7 +11,7 @@ const messages = {
         },
         "about": {
             "title": "About the {website}",
-            "content": "The Singapore Tax Calculatior is maintained by {author}. It was refer the {thanks_website} UI design and code base (that was maintained by {thanks_author} and Emi), If you find errors in those calculations, feel free to look into the code. It is being kept as simple as possible. If more changes need to be made, please feel free to create issue or pull request via {github}."
+            "content": "The Singapore Tax Calculatior is maintained by {author}. It was refer the {thanks_website} UI design and code base (that was maintained by {thanks_author} and Emi), the icons made by {freepik} from {flaticon}. If you find errors in those calculations, feel free to look into the code. It is being kept as simple as possible. If more changes need to be made, please feel free to create issue or pull request via {github}."
         },
         "disclaimer": {
             "title": "Disclaimer",
